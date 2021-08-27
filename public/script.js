b68815fdcffa50535304bd3a83d8f44b2b46b0b3
Daniel Susman <frenchminister@gmail.com>
2021-08-27 13:01:15 -0400

    Fix hardcoded HTML/JS for projects page

---
+++ public/script.js
@@ -0,0 +1,23 @@
+console.log("hi there from toggle");
+Array.from(document.getElementsByTagName("button")).forEach((e) => {
+  e.addEventListener("click", projectToggle);
+});
+
+function projectToggle(e) {
+  let project = e.target;
+  off(project);
+  let projectElement = document.getElementsByClassName(project.id)[0];
+  if (projectElement !== undefined) {
+    projectElement.classList.add("show");
+    document.getElementById(project.id).classList.add("active");
+  }
+}
+
+function off(project) {
+  Array.from(document.getElementsByClassName("p")).forEach((e) => {
+    e.classList.remove("show");
+  });
+  Array.from(document.getElementsByTagName("button")).forEach((e) => {
+    e.classList.remove("active");
+  });
+}
--- src/utils/script.js
+++
@@ -1,3 +0,0 @@
-// document.getElementsByClassName('p').forEach((e) => {
-//   e.classList.remove('show')});
-// document.getElementsByClassName('portfolio')[0].classList.add('show');
